cd0506e903df7da4fb8c4fbd5c66413b411a7717
Prasantha Fernando <fprasantha@gmail.com>
2017-12-23 18:37:41 -0500

    Welcome page changes

--- poc-routing/src/app/app.component.ts
+++ poc-routing/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle: string = 'Acme Product Management';
+
+  constructor() { }
+
+  logOut(): void {
+
+    console.log('Log out');
+  }
 }
